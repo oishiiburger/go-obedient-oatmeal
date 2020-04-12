e9ce80356c7d555bae6cd5b1ab4e3f4fdc3e72b6
Chris Graham <grahamchristopherc@gmail.com>
2020-04-12 04:06:13 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+
+Go Obedient Oatmeal
+Chris Graham, 2020
+https://github.com/oishiiburger/go-obedient-oatmeal
+
+
+This is the Go version of Obedient Oatmeal, a band name generator which parses
+an input text for nouns and adjectives to use in the naming.
+
+*/
+
+package main
